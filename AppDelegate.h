2b91d9bbb5c7cd23496ec7e85de6a13c3b838bfc
sayali1612 <junnarkarsayali@gmail.com>
2015-03-28 13:08:40 +0530

    1.Weather is a weather forecast app which accepts multiple city names from user . 2. Gives forecast for 14 days. 3.Finds current city using gps and gives its forecast.

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather
+//
+//  Created by betteruse on 3/26/15.
+//  Copyright (c) 2015 ___BetterLabs___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate,UINavigationControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
